7fdf6df419c769afb4d74ab184e953c73b2f3386
chh <chenhehao@oliveche.com>
2021-04-23 15:36:54 +0800

    重构和增强插件的功能

--- README.md
+++ README.md
@@ -22,14 +22,14 @@ Plugins::add("print");
 ### 打印
 
 1. 安装第三方打印应用
-   \\server-pc\share\software\光速云插件5.0.5.zip
+	\\server-pc\share\software\光速云插件5.1.3-win7.exe
 
 2. 准备打印模板文件, 放置到代码server/template目录下
 
 3. 后端需要支持打印的AC类中, 重载accessControl类里的onHandleExportFormat, 在该函数调用插件
-   return printSvc($fmt, $ret, $fname, $tpl);
+	return printSvc($fmt, $ret, $fname, $tpl);
 
-- fmt: 目前只支持excel, 所以fmt就是"excel"
+- fmt: 目前只支持excel, 所以fmt就是"excel" 
 - ret: 待打印的数据
 - fname: 输出文件名
 - tpl: 模板文件名称, 默认会在template目录下查找
@@ -42,7 +42,7 @@ Plugins::add("print");
 
 getShareUrl(ac, cond, fmt, fname, tpl)
 
-- fmt: 目前只支持excel, 所以fmt就是"excel"
+- fmt: 目前只支持excel, 所以fmt就是"excel" 
 - ret: 待打印的数据
 - fname: 输出文件名
 - tpl: 模板文件名称, 默认会在template目录下查找
@@ -83,9 +83,55 @@ GSCloudPlugin.GetPrinters({
 ```
 
 ## 语法支持
-1. 待替换的部分原则上用{}包含
-2. 字段名对应, 比如查询的字段名是 cmt, 那么模板里定义为{cmt}
-3. 支持`.`符号拼接变量, 比如需显示 数量(qty) + 单位(unit), 可使用 {qty.unit}
-4. 支持php函数调用, 比如可定义`{date(FMT_D,strtotime(productTm))}`, 会生成生产日期信息
-5. 支持子表, 使用`START_ROW::START_SEQ`标识子表起点位置. {子表名}来识别查询结果中的子表, 如START_ROW::START_SEQ{inv1}. 子表只需要定义第一行, 后面会自动循环和填入
-6. 支持二维码生成, 需标识IMG和二维码对应的字段, 比如{IMG::qrCodeFlag,itemCode,orderCode,batchNo,cartonNo,qty,bpCode,unit}, 会将这些字段拼接为字符串并生成二维码
++ 待处理的关键字或变量用`{}`包含
+
++ 模板内字段名与查询返回的字段名对应, 比如查询的字段名是`cmt`, 那么模板里定义为`{cmt}`
+
++ 支持变量和常量混用, 比如同一单元格内填入`{qty}件`, 会被替换为具体的数量, 比如 `10件`.
+
++ 同一单元格支持多变量, 比如`{qty}{unit}`, 会被替换为 `10件`.
+
++ 多个变量拼接. 可以使用`.`符号拼接变量, 比如需显示`qty` + `unit`, 可使用 `{qty.unit}` 
+
++ 支持php函数调用, 比如可定义`{date(FMT_D,strtotime(productTm))}`, 会生成生产日期信息
+
++ 支持for循环输出主表或者子表多行记录, 循环的起点使用`{j_for}`标识, 如果是子表, 那么使用`{j_for::子表名}`.多行输出只需要定义第一行, 插件会根据查询返回的行数自动填充行数
+比如:	
+	```
+	{j_for::inv1}{id}
+	```
+	标识该行是inv1子表的起点, 本列填入inv1子表的id字段
+
+	如果是多行主表,不写子表名即可, 比如
+	```
+	{j_for}{orderNo}
+	```
+	+ 对多行的情况支持自增加序号函数`autoNo()`, 比如首列需要填入从1开始的自增长行号:
+
+		```
+		{j_for}{autoNo()}
+		```
+
++ 图片插入. 使用`{j_img}`标识. 比如插入一张地址为`http://oliveche.com/print/1.jpg`的图片
+	```
+	{j_img}{'http://oliveche.com/print/1.jpg'}
+	```
+	类似于`<img src=xxxx.jpg>`的用法.
+
+	+ 网络地址需要用`http/https`开头
+	+ 如果是无需处理的常量字符串, 使用单引号 `'` 引起来
+
++ 二维码生成
+使用`qrcode(content)`函数生成二维码, 比如
+
+	```
+	{j_img}{qrcode(oliveche.com/jd_cloud/api/order.get?id=1)}
+	```
+	可以生成对应url的一张二维码
+
++ 二维码字段拼接: 
+使用`concatField(...args)`拼接字段
+二维码生成一种常见的情况是,使用查询的字段名和值拼接成一段字段串, 生成二维码, 比如想把itemCode,batchNo,cartonNo这三个字段及对应的值拼接成字符串,那么按如下实现:
+	```
+	{j_img}{qrcode(concatField(itemCode,batchNo,cartonNo))}
+	```
--- server/plugin/print/print.php
+++ server/plugin/print/print.php
@@ -1,206 +1,325 @@
 <?php
 
-/*! jdcloud-plugin-url BEGIN */
-function api_getShareUrl()
-{
-	$param = [
-		"get" => $_GET,
-		"post" => $_POST,
-		"ses" => session_id()
-	];
-	$p = json_encode($param, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-	//addLog($p);
-	return makeUrl(getBaseUrl() . "url.php", ["p"=>jdEncrypt($p)]);
-}
-/*! jdcloud-plugin-url END */
+class PrintUtil {
+	protected $ret;
+	protected $lastCol;
+	protected $lastRow;
+	protected $sheet;
+	protected $obj;
+	protected $startRow;
+	protected $endRow;
+	protected $tempVal;
 
-function printFile($fmt, $ret, $fname, $tpl)
-{
-	if ($fmt === "excel") {
-		header("Content-disposition: attachment; filename=" . $fname . ".xlsx");
-		header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-		header("Content-Transfer-Encoding: binary");
-	
-		$retArr = table2objarr($ret);
-	
-		require_once 'class/vendor/phpoffice/phpexcel/Classes/PHPExcel.php';
-		require_once 'class/vendor/phpoffice/phpexcel/Classes/PHPExcel/IOFactory.php';
+	public function __construct($sheet, $ret, $lastRow, $lastCol) {
+        $this->ret = $ret;
+        $this->sheet = $sheet;
+        $this->lastCol = $lastCol;
+        $this->lastRow = $lastRow;
+    }
 
-		// 模板
-		$file = "./template/$tpl.xlsx";
-		if (!file_exists($file)) {
-			throw new MyException(E_PARAM, "template file: $file not exist");
+	public function handleCell ($col, $row, &$lastRow) {
+		//获取原始值
+		$sheet = $this->sheet;
+		$cellValue = $sheet->getCellByColumnAndRow($col, $row)->getValue();
+		if (!isset($cellValue)) {
+			return;
 		}
-		//logit("start load excel file");
 
-		$reader = PHPExcel_IOFactory::createReader("Excel2007");
-		$spreadsheet = $reader->load($file);
+		addLog("row: $row, col: $col, cell ori value: " . $cellValue);
+		$this->cellValue = $cellValue;
+		$matched = false;
+		$isImg = false;
 
-		$sheet = $spreadsheet->getActiveSheet();
-		$writer = PHPExcel_IOFactory::createWriter($spreadsheet, 'Excel2007');
+		//解析
+		$expr = preg_replace_callback('/\{([^{}]+)\}/iU', function ($ms) use($col, $row, &$isImg, &$lastRow, &$matched) {
+			$m = $ms[0];
+			$matched = true;
 
-		// Get the highest row and column numbers referenced in the worksheet
-		$highestRow = $sheet->getHighestRow(); // e.g. 10
-		$highestColumn = $sheet->getHighestColumn(); // e.g 'F'
-		$highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);
+			addLog("parse first time- ms[0]: " . $ms[0] . " ms[1]: " . $ms[1]);
+			//logit("ms[1]: " . $ms[1] . " expr: " . $expr);
 
-		logit("maxrow: $highestRow; maxcol: $highestColumn");
+			$isTable = false;
+			$subObj = null;
+
+			$expr = preg_replace_callback('/(?<![0-9])\$?([a-z_]\w*\b)(?!\()/iU', function ($ms1) use($col, $row, &$lastRow, &$isImg, &$isTable, &$subObj) {
+				$expr1 = "";
+				switch($ms1[0]) {
+					case "j_for":
+						$isTable = true;
+						break;
+					case "j_img":
+						$isImg = true;
+						break;
+					case "FMT_D":
+						$expr1 = $ms1[0];
+						break;
+					default:
+						if ($isTable)
+							$subObj = $ms1[0];
+						else if ($isImg){
+							$expr1 = $ms1[0];
+						}
+						else {
+							$expr1 = '$r["' . $ms1[0] . '"]';
+						}
+						break;
+				}
+
+				logit("ms1[0]: " . $ms1[0] . " expr: " . $expr1);
+				return $expr1;
+			}, $ms[1]);
+
+			logit("ms[1]: " . $ms[1] . " expr: " . $expr);
+
+			$ret = null;
+
+			//根据一个{}里的信息可以判断对应的cell, 可以做cell类别和多行的处理了
+			if ($isTable) {
+				$expr = "";
+				$cnt = isset($subObj) ? count($this->ret[$subObj]) : count($this->ret);
+				$this->obj = isset($subObj) ? $this->ret[$subObj] : $ret;
+				$this->startRow = $row;	
+				$this->endRow = $row + $cnt;
+
+				logit("start add row");
+				//j-for这一个括号里的内容去掉
+				$tempVal = preg_replace_callback('/\{([^{}]+)\}/iU', function ($ms2) {
+					return "";
+				}, $this->cellValue, 1);
+
+				logit("temp val: " . $tempVal);
+				$this->sheet->setCellValueByColumnAndRow($col, $row, $tempVal);
+				$this->addRow($cnt, $row, $col);	
+				$lastRow = $this->endRow;
+
+				logit("end add row");
+			}
+			else if (isset($expr) && strlen($expr) > 0 && $matched) {
+				if (isset($this->obj))
+				{
+					if ($row <= $this->endRow) {
+						$r = $this->obj[$row - $this->startRow];
+					}
+					else {
+						$this->obj = null;
+					}
+				}
+				else {
+					$r = $this->ret;
+				}
+
+				logit("before eval: " . $expr);
+
+				$expr = "return (" . $expr . ");";
+
+				logit("full expr: " . $expr);
+
+				$ret = eval($expr);
+				if (!isset($ret)) {
+					$ret = "";
+				}
+				logit("after eval: " . $ret);
+			}
+
+			return $ret;
+		}, $cellValue);
+
+		if ($matched) {
+			if ($isImg) {
+				$this->setImg($expr, $col, $row); 
+			}
+			else {
+				$sheet->setCellValueByColumnAndRow($col, $row, $expr);
+			}
+		}
+	}
+
+	protected function addRow($cnt, $row, $col) {
+		logit($cnt . " - " . $row . " - " . $col);
+		if ($cnt <= 1)
+			return;
+
+		$newRow = $row + 1;
+
+		logit(000);
+		$sheet = $this->sheet;
+		$sheet->insertNewRowBefore($newRow, $cnt-1);
+
+		for($i=0; $i<$cnt-1; $i++){
+			for ($newCol = $col; $newCol <= $this->lastCol; ++$newCol) {
+				$cellValue = $sheet->getCellByColumnAndRow($newCol, $row)->getValue();
+
+				if (isset($cellValue)) {
+
+					logit(" set cell value: " . $cellValue);
+					$sheet->setCellValueByColumnAndRow($newCol, $newRow, $cellValue);
+				}
+			}
+			$newRow++;
+		}
+	}
+
+	//生成二维码并设置到对应的位置
+	protected function setImg($imgPath, $col, $row) {
+		logit("create img: " . $imgPath);
+
+		$sheet = $this->sheet;
+		//$drawing->setName('qrCode');
+
+		if (stripos($imgPath, "http") !== false) {
+			$img = @imagecreatefromjpeg($imgPath);
+			if($img == null || !$img) {
+				logit("failed.....");
+				return;
+			}
 		
-		if (isset($_GET["align"])) {
-			$align = PHPExcel_Style_Alignment::HORIZONTAL_LEFT;
-			switch($_GET["align"]) {
-				case middle:
-					$align = PHPExcel_Style_Alignment::HORIZONTAL_MIDDLE;
-					break;
-				case right:
-					$align = PHPExcel_Style_Alignment::HORIZONTAL_RIGHT;
-					break;
-				default:
-					break;
-			};
-
-			$styleArray = [
-				'alignment' => [
-				'horizontal' => $align,
-				],
-			];
-
-			$sheet->getStyle('A1:'.$highestColumn.$highestRow)->applyFromArray($styleArray);
+			$drawing =new PHPExcel_Worksheet_MemoryDrawing();
+			$drawing->setImageResource($img);
+			$drawing->setRenderingFunction(PHPExcel_Worksheet_MemoryDrawing::RENDERING_JPEG);//渲染方法
+			$drawing->setMimeType(PHPExcel_Worksheet_MemoryDrawing::MIMETYPE_JPEG);
+		}
+		else {
+			$drawing = new PHPExcel_Worksheet_Drawing();
+			$drawing->setPath($imgPath);
 		}
 
-		if (count($retArr) == 1) {
-			$r = $retArr[0];
-			logit($r);
+		//col起始位置是A, 就是65
+		$colName = chr(65 + $col) . $row;
 
-			$subRow = 0;
-			$subCol = 0;
-			$seqCol = 0;
-			$subobj = null;
+		$drawing->setHeight(74);
+		$drawing->setWidth(74);
+		$drawing->setCoordinates($colName);
+		$drawing->setOffsetX(15);
+		$drawing->setOffsetY(2);
+		$drawing->setWorksheet($sheet);
 
-			for ($row = 1; $row <= $highestRow; ++$row) {
-				if ($row == $subRow)
-					continue;
-
-				//这个循环用于处理主表的数据
-				for ($col = 0; $col <= $highestColumnIndex; ++$col) {
-					$cellValue = $sheet->getCellByColumnAndRow($col, $row)->getValue();
-
-					$match = [];
-					if (isset($cellValue)) {
-						if (stripos($cellValue, "START_SEQ") !== false) {
-							$seqCol = $col;
-							logit("start_seq: " . $seqCol);
-						}
-
-						$match = preg_replace('/.*{(.+)}.*/iu', '$1', $cellValue);
-						logit("matched value: " . $match);
-
-						if (stripos($cellValue, "START_ROW") !== false) {
-							$subRow = $row;
-							$subCol = $col;
-							$subobj = $match;
-
-							logit("subRow: " . $subRow . " subCol:" . $subCol . " subobj: " . $subobj);
-							break;
-						}
-
-						if ($match != $cellValue) {
-							$isImg = (stripos($match, "IMG::") !== false);
-							$ret = getField($match, $r, $isImg);
-
-							//生成和设置图片
-							if ($isImg) {
-								//生成二维码
-								include 'class/phpqrcode/phpqrcode.php'; 
-
-								logit("create img: " . $ret);
-								$errorCorrectionLevel = 'L';//容错级别
-								$matrixPointSize = 6;//生成图片大小
-								$imgPath = './qrcode.png';
-								QRcode::png($ret, $imgPath, $errorCorrectionLevel, $matrixPointSize, 2);
-
-								$drawing = new PHPExcel_Worksheet_Drawing();
-								//$drawing = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
-								$drawing->setName('qrCode');
-								$drawing->setPath($imgPath);
-
-								//col起始位置是A, 就是65
-								$colName = chr(65 + $col) . $row;
-
-								$drawing->setHeight(126);
-								$drawing->setWidth(126);
-								$drawing->setCoordinates($colName);
-								$drawing->setOffsetX(15);
-								$drawing->setOffsetY(2);
-								$drawing->setWorksheet($sheet);
-								$sheet->setCellValueByColumnAndRow($col, $row, " ");
-							}
-							else { //设置文字内容
-								logit("set field $col, $row, $ret");
-								$sheet->setCellValueByColumnAndRow($col, $row, $ret);
-							}
-						}
-					}
-				}
-			}
-
-			if (isset($subobj)) {
-				$subcnt = count($r[$subobj]);
-				//$sheet->insertNewRowBefore(7, 2);
-				logit("enter subobj cnt: " . $subcnt);
-
-				//根据子表行数来插入并复制子表第一行的信息
-				if ($subcnt > 1) {
-					$row = $subRow + 1;
-					$sheet->insertNewRowBefore($subRow+1, $subcnt-1);
-					for($i=0; $i<$subcnt-1; $i++){
-						for ($col = $subCol; $col <= $highestColumnIndex; ++$col) {
-							$cellValue = $sheet->getCellByColumnAndRow($col, $subRow)->getValue();
-
-							if (isset($cellValue)) {
-								$sheet->setCellValueByColumnAndRow($col, $row, $cellValue);
-							}
-						}
-						$row++;
-					}
-				}
-
-				$row = $subRow;
-				for($i=0; $i<$subcnt; $i++){
-					$sub = $r[$subobj][$i];
-
-					for ($col = $subCol; $col <= $highestColumnIndex; ++$col) {
-						$cellValue = $sheet->getCellByColumnAndRow($col, $row)->getValue();
-
-						$match = [];
-						if ($col == $seqCol) {
-							$sheet->setCellValueByColumnAndRow($col, $row, $i+1);
-						}
-						else if (isset($cellValue)) {
-							$match = preg_replace('/.*{(.+)}.*/iu', '$1', $cellValue);
-							logit("matched value: " . $match);
-
-							if ($match != $cellValue) {
-								$ret = getField($match, $sub);
-
-								logit("set field $col, $row, $ret");
-								$sheet->setCellValueByColumnAndRow($col, $row, $ret);
-							}
-						}
-					}
-
-					$row++;
-				}
-			}
-
-			$writer->save('php://output');
-		}
-
-		return true;
+		$sheet->setCellValueByColumnAndRow($col, $row, "");
 	}
 }
 
+function printFile($fmt, $ret, $fname, $tpl)
+{
+	if ($fmt != "excel") {
+		return;
+	}
+
+	header("Content-disposition: attachment; filename=" . $fname . ".xlsx");
+	header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+	header("Content-Transfer-Encoding: binary");
+
+	$retArr = table2objarr($ret);
+
+	require_once 'class/vendor/autoload.php';
+	require_once 'class/vendor/phpoffice/phpexcel/Classes/PHPExcel.php';
+	require_once 'class/vendor/phpoffice/phpexcel/Classes/PHPExcel/IOFactory.php';
+	require_once 'class/phpqrcode/phpqrcode.php';
+
+	//require_once "D:/work/wms/server/php/class/vendor/autoload.php";
+	//require_once "D:/work/wms/server/php/class/vendor/phpoffice/phpexcel/Classes/PHPExcel/Autoloader.php";
+	//require_once "D:/work/wms/server/php/class/vendor/phpoffice/phpexcel/Classes/PHPExcel.php";
+	//require_once "D:/work/wms/server/php/class/vendor/phpoffice/phpexcel/Classes/PHPExcel/IOFactory.php";
+	//require_once "D:/work/wms/server/php/class/phpqrcode/phpqrcode.php";
+
+	// 模板
+	$file = "./template/$tpl.xlsx";
+	if (!file_exists($file)) {
+		throw new MyException(E_PARAM, "template file: $file not exist");
+	}
+	//logit("start load excel file");
+
+	$reader = PHPExcel_IOFactory::createReader("Excel2007");
+	$spreadsheet = $reader->load($file);
+
+	$sheet = $spreadsheet->getActiveSheet();
+	$writer = PHPExcel_IOFactory::createWriter($spreadsheet, 'Excel2007');
+
+	// Get the highest row and column numbers referenced in the worksheet
+	$highestRow = $sheet->getHighestRow(); // e.g. 10
+	$highestColumn = $sheet->getHighestColumn(); // e.g 'F'
+	$highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);
+
+	logit("maxrow: $highestRow; maxcol: $highestColumn");
+	
+	if (isset($_GET["align"])) {
+		$align = PHPExcel_Style_Alignment::HORIZONTAL_LEFT;
+		switch($_GET["align"]) {
+			case middle:
+				$align = PHPExcel_Style_Alignment::HORIZONTAL_MIDDLE;
+				break;
+			case right:
+				$align = PHPExcel_Style_Alignment::HORIZONTAL_RIGHT;
+				break;
+			default:
+				break;
+		};
+
+		$styleArray = [
+			'alignment' => [
+			'horizontal' => $align,
+			],
+		];
+
+		$sheet->getStyle('A1:'.$highestColumn.$highestRow)->applyFromArray($styleArray);
+	}
+
+	if (count($retArr) == 1) {
+		logit($retArr[0]);
+		$print = new PrintUtil($sheet, $retArr[0], $highestRow, $highestColumnIndex);
+	}
+	else {
+		logit($retArr);
+		$print = new PrintUtil($sheet, $retArr, $highestRow, $highestColumnIndex);
+	}
+
+	for ($row = 1; $row <= $highestRow; ++$row) {
+		for ($col = 0; $col <= $highestColumnIndex; ++$col) {
+			$print->handleCell($col, $row, $highestRow);
+		}
+	}
+
+	$writer->save('php://output');
+
+	return true;
+}
+
+function autoNo() {
+	static $i = 0;
+	return ++$i;
+}
+
+function concatField() {
+	$numargs = func_num_args();
+    logit("Number of arguments: $numargs");
+
+	$concatStr = "";
+    $arg_list = func_get_args();
+    for ($i = 0; $i < $numargs; $i++) {
+		if ($i > 0) {
+			$concatStr .= '&';
+		}
+		$concatStr .= $arg_list[$i] . '=$r[' . $arg_list[$i] . ']';
+    }
+
+	return $concatStr;
+}
+
+function qrcode($str) {
+	$errorCorrectionLevel = 'L';//容错级别
+	$matrixPointSize = 6;//生成图片大小
+	$imgPath = './qrcode-' . autoNoPrivate() . '.png';
+	QRcode::png($str, $imgPath, $errorCorrectionLevel, $matrixPointSize, 2);
+
+	logit("image str: " . $str . " image path: " . $imgPath);
+
+	return $imgPath;
+}
+
+function autoNoPrivate() {
+	static $noPrivate = 0;
+	return ++$noPrivate;
+}
 function getField($raw, $r, $isImg=false) {
+
 	if ($isImg) {
 		$raw = str_replace("IMG::","",$raw);
 	}
@@ -244,4 +363,33 @@ function getField($raw, $r, $isImg=false) {
 	return $ret;
 }
 
+/*
+function api_testMatch() {
+	$src = '{a::b} + {b}';//mparam("src");
+
+//	$expr = preg_replace_callback('/(?<![0-9])\$?([a-z_]\w*\b)(?!\()/iU', function ($ms) {
+	$expr = preg_replace_callback('/\{([^{}]+)\}/iU', function ($ms) {
+		$m = $ms[0];
+	
+		addLog("start");
+		addLog($ms[0]);
+		addLog($ms[1]);
+		addLog("end");
+
+		$expr = preg_replace_callback('/(?<![0-9])\$?([a-z_]\w*\b)(?!\()/iU', function ($ms2) {
+			addLog("2start");
+			addLog($ms2[0]);
+			addLog($ms2[1]);
+			addLog("2end");
+			return ">2<";
+		}, $ms[1]);
+
+		addLog($expr);
+		//logit("ms[0]: " . $m . " expr: " . $expr);
+		return ">1<";
+	}, $src);
+
+	addLog($expr);
+}
+*/
 // vi: foldmethod=marker
